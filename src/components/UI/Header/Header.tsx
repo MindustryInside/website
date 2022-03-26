55f31eee969f9af4455f9270a17e1afa0523fc2f
Ademun <steamvaadim@gmail.com>
2022-03-26 17:44:08 +0300

    fixes

--- src/components/UI/Header/Header.tsx
+++ src/components/UI/Header/Header.tsx
@@ -7,13 +7,13 @@ function Header() {
   return (
     <header className='header'>
       <div className="inner__links">
-        <Link className="header__link" to={'website/main'}>
+        <Link className="header__link" to={'/main'}>
           <div className="link__to__main"></div>
         </Link>
-        <Link className="header__link" to={'website/chart'}>
+        <Link className="header__link" to={'/chart'}>
           График онлайна
         </Link>
-        <Link className="header__link" to={'website/pluginsnmods'}>
+        <Link className="header__link" to={'/pluginsnmods'}>
           Плагины и Моды
         </Link>
       </div>
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,4 +1,4 @@
 import ChartPage from "../pages/ChartPage"
 import MainPage from "../pages/MainPage"
 import PluginsNModsPage from "../pages/PluginsNModsPage"
-export const publicRoutes = [{path: '*', element: MainPage}, {path: 'website/chart', element: ChartPage}, {path: 'website/pluginsnmods', element: PluginsNModsPage}]
+export const publicRoutes = [{path: '*', element: MainPage}, {path: '/chart', element: ChartPage}, {path: '/pluginsnmods', element: PluginsNModsPage}]
